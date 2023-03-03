9ca96a5e15440706dd82a7945b0acd33655d7c1f
Narayana_Reddy <narayana.reddy@quencil.in>
2023-03-03 13:28:42 +0530

    added landing_page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from "./Navbar.js"
+import Previous from "./Previous.js"
+import Older from "./Older.js"
+
+function LandingPage() {
+    return (
+        <>
+            <Navbar />
+            <Previous />
+            <Older />
+        </>
+    )
+}
+
+export default LandingPage
